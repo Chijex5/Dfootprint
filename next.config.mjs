80a3826d7d1b8f99934ab95ad9a01c6af48c16cc
Chijioke Uzodinma <embroconnect3@gmail.com@gmail.com>
2025-01-19 16:39:31 +0100

    trying to fix the image problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['drive.google.com', 'https://drive.usercontent.google.com/'], // Add your image hosting domain here
+        domains: ['drive.google.com'],
     },
 };
 
