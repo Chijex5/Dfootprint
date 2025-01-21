4489b4a3afe441ebcc2c8bf240ec1bbda6b32047
Chijioke Uzodinma <embroconnect3@gmail.com@gmail.com>
2025-01-21 08:09:07 +0100

    Did some clean up and UI improvement

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{html,js,jsx,ts,tsx,css}',
   ],
   darkMode: "class", // Enable dark mode with the "class" strategy
   theme: {
